9525f1ff90ae9281852a0d09a79e7e4d4d33dfdd
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-07-22 00:31:41 -0300

    feat: babel dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import app from "app"
+import app from "@/app"
 
 app.listen(process.env.PORT || 5000, () =>
 	console.log(`Server running on port ${process.env.PORT || 5000} 🚀`)
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,6 @@
 describe("just test", () => {
-  it("should be 4", () => {
-    const sum = 2 + 2;
-    expect(sum).toBe(4);
-  });
-});
+	it("should be 4", () => {
+		const sum = 2 + 2
+		expect(sum).toBe(4)
+	})
+})

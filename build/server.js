3b77bf3316771b508b6b1a10b7f887ecd402a35c
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-07-22 08:49:45 -0300

    build folder deleted

--- build/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _app = require("app.js");
-
-const {
-  app
-} = new _app.App();
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
